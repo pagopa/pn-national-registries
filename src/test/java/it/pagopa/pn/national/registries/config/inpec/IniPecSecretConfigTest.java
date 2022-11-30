bb5061459c2d4abe8f865b4dbdd5bcffa0b80c56
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2022-11-30 15:43:14 +0100

    test push

---
+++ src/test/java/it/pagopa/pn/national/registries/config/inpec/IniPecSecretConfigTest.java
@@ -0,0 +1,10 @@
+package it.pagopa.pn.national.registries.config.inpec;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class IniPecSecretConfigTest {
+
+}
