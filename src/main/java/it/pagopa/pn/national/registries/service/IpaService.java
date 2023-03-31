4b2b4f468cc3812e686db9670ded786bfde08666
BaldiVi <vittorio.baldi@emea.nttdata.com>
2023-03-31 16:29:16 +0200

    Rimossi import inutilizzati

--- src/main/java/it/pagopa/pn/national/registries/service/IpaService.java
+++ src/main/java/it/pagopa/pn/national/registries/service/IpaService.java
@@ -3,7 +3,6 @@ package it.pagopa.pn.national.registries.service;
 import it.pagopa.pn.national.registries.client.ipa.IpaClient;
 import it.pagopa.pn.national.registries.converter.IpaConverter;
 import it.pagopa.pn.national.registries.exceptions.PnNationalRegistriesException;
-import it.pagopa.pn.national.registries.generated.openapi.rest.v1.dto.IPAPecDto;
 import it.pagopa.pn.national.registries.generated.openapi.rest.v1.dto.IPAPecErrorDto;
 import it.pagopa.pn.national.registries.generated.openapi.rest.v1.dto.IPAPecOKDto;
 import it.pagopa.pn.national.registries.generated.openapi.rest.v1.dto.IPARequestBodyDto;
@@ -14,7 +13,6 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.nio.charset.Charset;
-import java.util.List;
 
 @Service
 @Slf4j
