42826c719a437e4b8de17208e3b896a27e3ab337
Simone Martelli <simone.martelli@emea.nttdata.com>
2023-04-11 18:25:38 +0200

    fix cache access token

--- src/main/java/it/pagopa/pn/national/registries/cache/AccessTokenExpiringMap.java
+++ src/main/java/it/pagopa/pn/national/registries/cache/AccessTokenExpiringMap.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
@@ -19,10 +20,9 @@ public class AccessTokenExpiringMap {
     private final TokenProvider tokenProvider;
 
     protected ExpiringMap<String, AccessTokenCacheEntry> expiringMap = ExpiringMap.builder()
-            .asyncExpirationListener((purposeId, accessTokenEntry) -> log.info("token for {} has expired", purposeId))
+            .asyncExpirationListener((purposeId, entry) -> log.info("token for purposeId {} has expired", purposeId))
             .variableExpiration()
             .build();
-
 
     public AccessTokenExpiringMap(TokenProvider tokenProvider, @Value("${pn.national-registries.pdnd.token.deadline}") Integer deadline) {
         this.tokenProvider = tokenProvider;
@@ -32,26 +32,29 @@ public class AccessTokenExpiringMap {
     public Mono<AccessTokenCacheEntry> getToken(String purposeId, PdndSecretValue pdndSecretValue) {
         if (expiringMap.isEmpty() || !expiringMap.containsKey(purposeId)) {
             return requireNewAccessToken(purposeId, pdndSecretValue);
-        } else {
+        }
+        try {
             long expiration = expiringMap.getExpectedExpiration(purposeId);
             if (expiration <= deadline) {
                 return requireNewAccessToken(purposeId, pdndSecretValue);
             } else {
-                log.info("Existing Access Token Required with PurposeId: " + purposeId);
+                log.info("Existing Access Token Required with purposeId: {}", purposeId);
                 return Mono.just(expiringMap.get(purposeId));
             }
+        } catch (NoSuchElementException e) {
+            return requireNewAccessToken(purposeId, pdndSecretValue);
         }
     }
 
     private Mono<AccessTokenCacheEntry> requireNewAccessToken(String purposeId, PdndSecretValue pdndSecretValue) {
-        log.info("New Access Token Required with PurposeId: " + purposeId);
+        log.info("New Access Token Required with purposeId: {}", purposeId);
         return tokenProvider.getTokenPdnd(pdndSecretValue)
-                .map(clientCredentialsResponseDto -> {
-                    AccessTokenCacheEntry tok = new AccessTokenCacheEntry(purposeId);
-                    tok.setClientCredentials(clientCredentialsResponseDto);
-                    expiringMap.put(purposeId, tok);
-                    expiringMap.setExpiration(purposeId, clientCredentialsResponseDto.getExpiresIn(), TimeUnit.SECONDS);
-                    return tok;
+                .map(dto -> {
+                    AccessTokenCacheEntry entry = new AccessTokenCacheEntry(purposeId);
+                    entry.setClientCredentials(dto);
+                    expiringMap.put(purposeId, entry);
+                    expiringMap.setExpiration(purposeId, dto.getExpiresIn(), TimeUnit.SECONDS);
+                    return entry;
                 });
     }
 }
