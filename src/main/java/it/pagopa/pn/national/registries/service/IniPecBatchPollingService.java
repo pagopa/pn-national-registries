e5b8668b0a585f97defdc8e99b20299ca0679539
marcello.satta <marcello.satta@nttdata.com>
2023-05-18 16:19:30 +0200

    - (Code clean-up) fix messaggio di risposta IniPEC "List PEC in progress." trattato come evento naturale e non come eccezione

--- src/main/java/it/pagopa/pn/national/registries/service/IniPecBatchPollingService.java
+++ src/main/java/it/pagopa/pn/national/registries/service/IniPecBatchPollingService.java
@@ -29,7 +29,6 @@ import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.*;
 import java.util.function.Function;
-import java.util.function.Predicate;
 
 import static it.pagopa.pn.commons.log.MDCWebFilter.MDC_TRACE_ID_KEY;
 import static it.pagopa.pn.national.registries.exceptions.PnNationalRegistriesExceptionCodes.ERROR_MESSAGE_INIPEC_RETRY_EXHAUSTED_TO_SQS;
