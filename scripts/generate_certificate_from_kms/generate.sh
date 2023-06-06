5fe6c1fd17a2d40b1db933da4cd10d6eb3599d36
Marco Iannaccone <marco.iannaccone@pagopa.it>
2023-06-06 18:26:45 +0200

    PN-6136: parameter store: string instead of securestring, no explicit profile, no e-mail in CSR

--- scripts/generate_certificate_from_kms/generate.sh
+++ scripts/generate_certificate_from_kms/generate.sh
@@ -2,12 +2,15 @@
 #   aws sso login --profile sso_pn-core-dev
 #
 # command must be in the form (sudo is needed for certbot):
-#   sudo ./generate.sh --fqdn cert4.dev.notifichedigitali.it --profile sso_pn-core-dev --region eu-south-1 --keyid 50431e00-79d4-4966-ad70-881d965bdb07
+#   sudo ./generate.sh --fqdn cert6.dev.notifichedigitali.it --profile sso_pn-core-dev --region eu-south-1 --keyid 50431e00-79d4-4966-ad70-881d965bdb07
 #
 # see generated certificate request with:
 #   openssl req -noout -text -in LOCAL_CSR.csr
 #   openssl req -noout -text -in new.csr
 # or with: https://www.digicert.com/ssltools/view-csr/
+#
+# examine the generated certificate file with:
+#   openssl x509 -in 0000_cert.pem -text -noout
 
 # Python part:
 #   python -m venv aws-kms-sign-csr/venv
@@ -15,19 +18,21 @@
 #   pip install -r aws-kms-sign-csr/requirements.txt
 
 
+# uncomment and set the profile name for executing locally, or make the wanted profile the default one
+#export AWS_PROFILE=sso_pn-core-dev
+
 # Check if the user has provided a FQDN #and a passphrase
 if [ $# -ne 8 ]; then
-    echo "Usage: ./generate.sh --fqdn <FQDN> --profile <PROFILE> --region <REGION> --keyid <KEYID>"
+    echo "Usage: ./generate.sh --fqdn <FQDN> --region <REGION> --keyid <KEYID>"
     exit 1
 fi
 
 # read variable data from the command line
 FQDN=$2
-PROFILE=$4
 REGION=$6
 KEYID=$8
 
-# fixed parameterså
+# fixed parameters
 CSR_FILE=LOCAL_CSR.csr
 PRIVATE_KEY_FILE=PRIVATEKEY.key
 PASSPHRASE=test
@@ -38,47 +43,12 @@ FIXED=/C=IT/ST=Italy/L=Rome/O=PagoPA/OU=SEND
 openssl req -newkey rsa:2048 -keyout ${PRIVATE_KEY_FILE} -out ${CSR_FILE} -subj ${FIXED}/CN=${FQDN} -passout pass:${PASSPHRASE}
 
 # sign the CSR with AWS KMS (https://github.com/g-a-d/aws-kms-sign-csr)
-python3 aws-kms-sign-csr/aws-kms-sign-csr.py --region ${REGION} --profile ${PROFILE} --keyid ${KEYID} --hashalgo sha256 --signalgo RSA ${CSR_FILE} > ${NEW_CSR_FILE}
+python3 aws-kms-sign-csr/aws-kms-sign-csr.py --region ${REGION} --keyid ${KEYID} --hashalgo sha256 --signalgo RSA ${CSR_FILE} > ${NEW_CSR_FILE}
 
-#read ~/.aws/config file and get, in the content, the directive with [profile ${PROFILE}]
-config_file=~/.aws/config
-target_block="[profile ${PROFILE}]"
-# set a flag to indicate if the target block is found
-found_block=false
-# read the file line by line
-echo "" > temp_aws_config_file
-while IFS= read -r line; do
-    # sheck if the current line matches the target block
-    if [[ $line == "$target_block" ]]; then
-        # set the flag to indicate that the target block is found
-        found_block=true
-    elif [[ $line == [* ]]; then
-        # check if the current line starts a new block
-        if [[ $found_block == true ]]; then
-            # if the target block has already been found and a new block is encountered,
-            # break the loop as we have reached the end of the target block
-            break
-        fi
-    fi
-
-    # if the target block is found, print the current line to the file
-    if [[ $found_block == true ]]; then
-        # if the line is equal to the target block, print it to the file
-        # print [profile default] to the file
-        if [[ $line == "$target_block" ]]; then
-            echo "[default]" >> temp_aws_config_file
-        else
-            echo "$line" >> temp_aws_config_file
-        fi
-    fi
-done < "$config_file"
-
-export AWS_CONFIG_FILE=temp_aws_config_file
-# there's also AWS_PROFILE
 # create the certificate from the certificate request, creating records on AWS Route53
     # comment these two lines for avoiding multiple requests to certbot (not two many for the same FQDN are allowed)
-#rm -f *.pem
-#certbot certonly --csr ${NEW_CSR_FILE} --dns-route53 -d ${FQDN}
+rm -f *.pem
+certbot certonly --csr ${NEW_CSR_FILE} --dns-route53 -d ${FQDN}
 
 # Requesting a certificate for cert.dev.notifichedigitali.it
 #Successfully received certificate.
@@ -86,9 +56,6 @@ export AWS_CONFIG_FILE=temp_aws_config_file
 #Intermediate CA chain is saved at:  /Users/marcoiannaccone/dev/work/PagoPA/testcert/0000_chain.pem
 #Full certificate chain is saved at: /Users/marcoiannaccone/dev/work/PagoPA/testcert/0001_chain.pem
 #This certificate expires on 2023-09-04.
-
-# examinate the certificate file with:
-#   openssl x509 -in 0000_cert.pem -text -noout
 
 # if file exists...
 if [ -f "0000_cert.pem" ]; then
@@ -107,21 +74,9 @@ if [ -f "0000_cert.pem" ]; then
 
     # convert the file to base64 and send that to AWS parameter store
         # -i works on Linux and Mac, without -i
-    aws ssm put-parameter --name "/certificates/${FQDN}/cert" --type "SecureString" --value "${parameter_json}" --overwrite --region ${REGION} # now default profile
-
-    # send the file to AWS ACM
-    # PROBLEM: the private key can't be retrieved from KMS and passed to the next command
-    #aws acm import-certificate --certificate fileb://0000_cert.pem --private-key fileb://kms_key_file.pem --certificate-chain fileb://0001_chain.pem --region ${REGION}
+    aws ssm put-parameter --name "/certificates/${FQDN}/cert" --type "String" --value "${parameter_json}" --overwrite --region ${REGION} # now default profile
 fi
-
 
 # profile rimosso
 # leggere parameter store per keyId e dns
-# aws ssm --profile sso_pn-core-dev --region eu-south-1 get-parameter --name /certificates/cert1.dev.notifichedigitali.it/cert --output json --no-paginate | 
 # aws ssm --profile sso_pn-core-dev --region eu-south-1 get-parameter --name /certificates/cert1.dev.notifichedigitali.it/cert --output json --no-paginate | jq -r .Parameter.Name
-# parameter no securestring
-# rimuovere mail
-
-
-# chiave kms da leggere:
-
