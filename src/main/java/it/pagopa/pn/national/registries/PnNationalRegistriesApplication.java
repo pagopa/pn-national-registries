c791b3e5ebc17a2b192f5fd134daaef268fbb25d
CapolongoDa <daniele.capolongo@emeal.nttdata.com>
2024-10-29 16:46:08 +0100

    PN-9464 - added SpringAnalyzer

--- src/main/java/it/pagopa/pn/national/registries/PnNationalRegistriesApplication.java
+++ src/main/java/it/pagopa/pn/national/registries/PnNationalRegistriesApplication.java
@@ -1,5 +1,6 @@
 package it.pagopa.pn.national.registries;
 
+import it.pagopa.pn.commons.configs.listeners.TaskIdApplicationListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,6 +11,9 @@ public class PnNationalRegistriesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PnNationalRegistriesApplication.class, args);
+        SpringApplication app = new SpringApplication(PnNationalRegistriesApplication.class);
+        app.addListeners(new TaskIdApplicationListener());
+        app.run(args);
     }
 
 }
