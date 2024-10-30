4d28709a5dbb307dbe54b52545aa689779a64ff8
CapolongoDa <daniele.capolongo@emeal.nttdata.com>
2024-10-30 12:42:15 +0100

    PN-9464 - added jUnit test

--- src/main/java/it/pagopa/pn/national/registries/PnNationalRegistriesApplication.java
+++ src/main/java/it/pagopa/pn/national/registries/PnNationalRegistriesApplication.java
@@ -1,19 +1,16 @@
 package it.pagopa.pn.national.registries;
 
-import it.pagopa.pn.commons.configs.listeners.TaskIdApplicationListener;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import static it.pagopa.pn.national.registries.utils.SpringApplicationUtils.buildSpringApplicationWithListener;
 
 @SpringBootApplication
 @EnableScheduling
 public class PnNationalRegistriesApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PnNationalRegistriesApplication.class, args);
-        SpringApplication app = new SpringApplication(PnNationalRegistriesApplication.class);
-        app.addListeners(new TaskIdApplicationListener());
-        app.run(args);
+        buildSpringApplicationWithListener().run(args);
     }
 
 }
