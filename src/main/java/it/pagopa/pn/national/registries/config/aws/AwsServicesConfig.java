d61f267166eaab06abba04ef871a0247e6cf6bbb
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2024-12-09 14:06:48 +0100

    Added localstack configuration

--- src/main/java/it/pagopa/pn/national/registries/config/aws/AwsServicesConfig.java
+++ src/main/java/it/pagopa/pn/national/registries/config/aws/AwsServicesConfig.java
@@ -4,49 +4,35 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.cloudwatch.CloudWatchAsyncClient;
 import software.amazon.awssdk.services.kms.KmsClient;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
-import software.amazon.awssdk.services.ssm.SsmClient;
 
 @Configuration
 @Slf4j
-public class AwsServicesClientsConfig {
+public class AwsServicesConfig {
 
     private final String region;
 
-    public AwsServicesClientsConfig(@Value("${aws.region}") String region) {
+    public AwsServicesConfig(@Value("${aws.region}") String region) {
         this.region = region;
     }
 
     @Bean
+    @Profile("default")
     public KmsClient kmsClient() {
         return KmsClient.builder()
                 .region(Region.of(region))
                 .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
-    @Bean
-    public CloudWatchAsyncClient cloudWatchClient() {
-        return CloudWatchAsyncClient.builder()
-                .region(Region.of(region))
-                .credentialsProvider(DefaultCredentialsProvider.create())
-                .build();
-    }
 
     @Bean
+    @Profile("default")
     public SecretsManagerClient secretsManagerClient() {
         return SecretsManagerClient.builder()
-                .region(Region.of(region))
-                .credentialsProvider(DefaultCredentialsProvider.create())
-                .build();
-    }
-
-    @Bean
-    public SsmClient ssmClient() {
-        return SsmClient.builder()
                 .region(Region.of(region))
                 .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
