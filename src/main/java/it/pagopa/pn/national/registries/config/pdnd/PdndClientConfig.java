9f7e27a8f293fe9d156c9e8f883d6c137bb7f3fc
empassaro <emanuele.passaro@emeal.nttdata.com>
2025-01-13 10:42:05 +0100

    [PN-13642] feat: added env to increase codec buffer memory size for every webclient

--- src/main/java/it/pagopa/pn/national/registries/config/pdnd/PdndClientConfig.java
+++ src/main/java/it/pagopa/pn/national/registries/config/pdnd/PdndClientConfig.java
@@ -8,15 +8,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 @RequiredArgsConstructor
 @Slf4j
 public class PdndClientConfig extends CommonBaseClient {
 
+    private final WebClient.Builder builder;
+
     @Bean
     AuthApi authApi(@Value("${pn.national.registries.pdnd.base-path}") String basePath) {
-        var apiClient = new ApiClient(initWebClient(ApiClient.buildWebClientBuilder()));
+        var apiClient = new ApiClient(initWebClient(this.builder));
         apiClient.setBasePath(basePath);
         return new AuthApi(apiClient);
     }
