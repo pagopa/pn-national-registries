43d3d96ad4ad62c090b5250cbb4bb12f7723e0e7
empassaro <emanuele.passaro@emeal.nttdata.com>
2025-01-14 13:51:03 +0100

    [PN-13417] fix: made optional endpoint url configuration for aws services clients

--- src/main/java/it/pagopa/pn/national/registries/config/aws/AwsServicesClientsConfigActivation.java
+++ src/main/java/it/pagopa/pn/national/registries/config/aws/AwsServicesClientsConfigActivation.java
@@ -3,12 +3,15 @@ package it.pagopa.pn.national.registries.config.aws;
 import it.pagopa.pn.commons.configs.RuntimeMode;
 import it.pagopa.pn.commons.configs.aws.AwsConfigs;
 import it.pagopa.pn.commons.configs.aws.AwsServicesClientsConfig;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.kms.KmsClient;
+import software.amazon.awssdk.services.kms.KmsClientBuilder;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClientBuilder;
 
 import java.net.URI;
 
@@ -23,20 +26,36 @@ public class AwsServicesClientsConfigActivation extends AwsServicesClientsConfig
 
     @Bean
     public KmsClient kmsClient() {
-        return KmsClient.builder()
-                .region(Region.of(props.getRegionCode()))
-                .endpointOverride(URI.create(props.getEndpointUrl()))
-                .credentialsProvider(DefaultCredentialsProvider.create())
-                .build();
+        KmsClientBuilder builder = KmsClient.builder()
+                .credentialsProvider(DefaultCredentialsProvider.create());
+
+        String regionCode = props.getRegionCode();
+        if (StringUtils.isNotBlank(regionCode)) {
+            builder.region(Region.of(regionCode));
+        }
+
+        String endpointUrl = props.getEndpointUrl();
+        if (StringUtils.isNotBlank(endpointUrl)) {
+            builder.endpointOverride(URI.create(endpointUrl));
+        }
+        return builder.build();
     }
 
     @Bean
     public SecretsManagerClient secretsManagerClient() {
-        return SecretsManagerClient.builder()
-                .region(Region.of(props.getRegionCode()))
-                .endpointOverride(URI.create(props.getEndpointUrl()))
-                .credentialsProvider(DefaultCredentialsProvider.create())
-                .build();
+        SecretsManagerClientBuilder builder = SecretsManagerClient.builder()
+                .credentialsProvider(DefaultCredentialsProvider.create());
+
+        String regionCode = props.getRegionCode();
+        if (StringUtils.isNotBlank(regionCode)) {
+            builder.region(Region.of(regionCode));
+        }
+
+        String endpointUrl = props.getEndpointUrl();
+        if (StringUtils.isNotBlank(endpointUrl)) {
+            builder.endpointOverride(URI.create(endpointUrl));
+        }
+        return builder.build();
     }
 
 }
