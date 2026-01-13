172cff9db88205e9f33e383bcd09b18ec83b59d5
federica.moschese <federica.moschese@emeal.nttdata.com>
2026-01-13 12:43:19 +0100

    [PN-17742] - Updated openAPI's with format type password; added ConsumerMDCUtils class; updated pom

--- src/main/java/it/pagopa/pn/national/registries/middleware/queue/consumer/GatewayInputsHandler.java
+++ src/main/java/it/pagopa/pn/national/registries/middleware/queue/consumer/GatewayInputsHandler.java
@@ -4,12 +4,13 @@ import io.awspring.cloud.sqs.annotation.SqsListener;
 import it.pagopa.pn.commons.utils.MDCUtils;
 import it.pagopa.pn.national.registries.middleware.queue.consumer.event.PnAddressGatewayEvent;
 import it.pagopa.pn.national.registries.service.GatewayService;
+import it.pagopa.pn.national.registries.utils.ConsumerMDCUtils;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.MDC;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 @lombok.CustomLog
 @RequiredArgsConstructor
 public class GatewayInputsHandler {
@@ -21,6 +22,7 @@ public class GatewayInputsHandler {
     public void pnNationalRegistriesGatewayRequestConsumer(Message<PnAddressGatewayEvent.Payload> message) {
         log.logStartingProcess(HANDLER_PROCESS);
         log.debug(HANDLER_PROCESS + "- message received for correlationId: {}", message.getPayload().getCorrelationId());
+        ConsumerMDCUtils.addMessageHeadersToMDC(message.getHeaders());
         MDC.put(MDCUtils.MDC_PN_CTX_REQUEST_ID, message.getPayload().getCorrelationId());
 
         var monoResult = gatewayService.handleMessage(message.getPayload())
